38d58f3c7488129e2d1c3a23c6b7a401786cb585
Harpreet Singh <hurpreet@icloud.com>
2023-05-11 05:45:29 -0400

    Created a random quote generator which will automatically loads a quotes when the HTML page is loaded. It will fetch the quote directly from public api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-console.log("I am in sync with HTML")
 
-document.addEventListener('DOMContentLoaded', () => {console.log("Hello from within the DOM")})
+//Generate a random quote from any anime using the public api
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    fetch("https://animechan.vercel.app/api/random")
+    .then((response) => response.json())
+    .then(data => document.getElementById("randomQuote").value = data.quote + "-" +data.character);
+  
+})
+
+//Generate a random quote from a particular anime
+
+// 1. Create ul representing names of animes 
+
+const quotesFromAnime= document.querySelectorAll(".quotes-from-anime")
+
+fetch('http://localhost:3000/ramens')
+  .then(response => response.json())
+  .then(data =>
+   renderRamens(data)
+  )
+  .catch(error => console.error('Error fetching ramen data:', error));
+
+
+
+
+
+
+
+
+     
+
+
